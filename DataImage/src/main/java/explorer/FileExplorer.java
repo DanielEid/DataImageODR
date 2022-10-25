49fa33161b72d6ba9c65da1d7bd03ae3f0abf463
Daniel <danieleid06@gmail.com>
2022-10-25 17:58:36 +0200

    Add recursive function to explore directory

--- DataImage/src/main/java/explorer/FileExplorer.java
+++ DataImage/src/main/java/explorer/FileExplorer.java
@@ -2,11 +2,13 @@ package explorer;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class FileExplorer {
 
@@ -16,34 +18,73 @@ public class FileExplorer {
         this.directory = directory;
     }
 
+    public String getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(String directory) {
+        this.directory = directory;
+    }
+
     @Override
     public String toString() {
-        return "FileExplorer{" +
-                "directory='" + directory + '\'' +
-                '}';
+        return "FileExplorer{" + "directory='" + directory + '\'' + '}';
     }
 
-    //Functions
+    /**
+     * Function of listing
+     * from https://howtodoinjava.com/java8/java-8-list-all-files-example/#1-listing-files-only-in-a-given-directory
+     */
 
-
-    public String getAppCurrentPath(){
+    public String getAppCurrentPath() {
         return System.getProperty("user.dir");
     }
-    public String getFilesInDirectory(){
 
-// Reading only files in the directory
-        try {
-            List<File> files = Files.list(Paths.get(this.directory))
-                    .map(Path::toFile)
-                    .filter(File::isFile)
-                    .collect(Collectors.toList());
+    /**
+     * @info: Print the list of files in this. directory
+     * Code inspired by part 1.2 of the website
+     */
+    public void getFilesInOneDirectory() {
+        List<File> fileList = new ArrayList<>();
 
-            files.forEach(System.out::println);
+        try (DirectoryStream<Path> stream = Files
+                .newDirectoryStream(Paths.get(directory))) {
+            for (Path path : stream) {
+                if (!Files.isDirectory(path)) {
+                    fileList.add(path.toFile());
+                }
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
+        fileList.forEach(System.out::println);
+    }
 
-        return "d";
+
+    //Recursive functions
+
+    private void pouet() {
+
+    }
+
+    public void getFilesInDirectories(String currentDirectory) {
+        List<File> fileList = new ArrayList<>();
+
+        try (DirectoryStream<Path> stream = Files
+                .newDirectoryStream(Paths.get(currentDirectory))) {
+            for (Path path : stream) {
+                if (!Files.isDirectory(path)) {
+                    fileList.add(path.toFile());
+                }
+                else {  //WARN WHEN EXCEPTION TODO FIX FRER
+                    if(Files.isDirectory(path))
+                        getFilesInDirectories(path.toString());
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        fileList.forEach(System.out::println);
     }
 
 }
--- DataImage/src/main/java/org/example/Main.java
+++ DataImage/src/main/java/org/example/Main.java
@@ -4,11 +4,11 @@ import explorer.FileExplorer;
 
 public class Main {
     public static void main(String[] args) {
-        FileExplorer fileExplorer= new FileExplorer("C:/Users/Daniel/Videos/Captures");
-        
-        System.out.println("current path: "+ fileExplorer.getAppCurrentPath());
-        fileExplorer.getFilesInDirectory();
+        FileExplorer fileExplorer = new FileExplorer("C:/Users/Daniel/Videos/Captures");
+        //TODO test with wrong path
 
-
+        System.out.println("current path: " + fileExplorer.getAppCurrentPath());
+        //fileExplorer.getFilesInOneDirectory();
+        fileExplorer.getFilesInDirectories(fileExplorer.getDirectory());
     }
 }
