3d50404d6348c854f7d0329b53ee8e38e2ae9f5c
Daniel <danieleid06@gmail.com>
2022-11-04 15:35:18 +0100

    Change file get method

--- DataImage/src/main/java/org/example/Main.java
+++ DataImage/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import com.google.gson.Gson;
 import fileControler.FileEditor;
 import fileControler.FileExplorer;
 
@@ -25,7 +26,11 @@ public class Main {
          */
         //System.out.println("current path: " + fileExplorer.getAppCurrentPath()); //Print current path
         List<File> fileList = fileExplorer.getFilesInDirectories(fileExplorer.getDirectory()); //Save the path of wanted files
-        String fileListString = fileList.toString(); //Transform it into string to save in file
+        StringBuilder fileListString = new StringBuilder();
+        for (int i = 0; i < fileList.size() - 1; i++) { //Transform the list into string, many way tested, this is the more light and not add [] (like toString()
+            fileListString.append(fileList.get(i) + ",");
+        }
+        fileListString.append(fileList.get(fileList.size() - 1));
 
         /*
          * File test editor (Write into file and read into file)
@@ -34,22 +39,27 @@ public class Main {
          * 3- Transform the String in a List<String> with split
          * 4- Foreach the List<String> and for each item create a file with the path
          */
-        fileEditor.writeStringIntoFile(filePathOfIndexedFilePath, fileListString); //Write data to a file given by the path
-        String dataReadedFromFile = fileEditor.readDataAsStringFile(filePathOfIndexedFilePath); // Get the data in a file type string
-        String[] myList = dataReadedFromFile.split(",");
+        fileEditor.writeStringIntoFile(filePathOfIndexedFilePath, String.valueOf(fileListString)); // 1-Write data to a file given by the path
+        String dataReadedFromFile = fileEditor.readDataAsStringFile(filePathOfIndexedFilePath); // 2- Get the data in a file type string
+        String[] dataReadedFromFileSplited = dataReadedFromFile.split(","); //3-
 
-        List<File> fileListFromFile = new ArrayList<>();
-        for (String item : myList) {
+        List<File> fileListFromFile = new ArrayList<>(); //4
+        for (String item : dataReadedFromFileSplited) {
             fileListFromFile.add(new File(item));
         }
+
         System.out.println(fileListFromFile);
-
-
-        //TODO fix exception when path is wrong
+        //TODO fix exception when path is wrong for write and read
+        //TODO see with gson
     }
 
     public static void main(String[] args) {
         testApp();
 
+        /*
+        long startTime = System.currentTimeMillis(); //TIMER STARTTTTTTTTTTTTTTTTTT
+        long finishTime = System.currentTimeMillis(); //TIMER ENDDDDDDDDDDDDDDDDDDD
+        System.out.println("That took: " + (finishTime - startTime) + " ms"); //TIMER OUTTTTTTTTTTTTTTTTT
+        */
     }
 }
