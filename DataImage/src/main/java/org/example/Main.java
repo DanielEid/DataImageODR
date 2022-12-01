ae8f96c4d111cdce8844ecc34fef5fd2a682fc73
Daniel <danieleid06@gmail.com>
2022-12-01 13:36:12 +0100

    Split code in main function + add OCR on a list of image

--- DataImage/src/main/java/org/example/Main.java
+++ DataImage/src/main/java/org/example/Main.java
@@ -1,8 +1,9 @@
 package org.example;
 
-import com.google.gson.Gson;
 import fileControler.FileEditor;
 import fileControler.FileExplorer;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -10,6 +11,45 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+
+    /**
+     * File research test
+     * This part create a list of file searched and transform it into a String
+     */
+    private static void writeFileListIntoFileAsString(FileExplorer fileExplorer, FileEditor fileEditor, String filePathOfIndexedFilePath) {
+        List<File> fileList = fileExplorer.getFilesInDirectories(fileExplorer.getDirectory()); //Save the path of wanted files
+        StringBuilder fileListString = new StringBuilder();
+        for (int i = 0; i < fileList.size() - 1; i++) { //Transform the list into string, many way tested, this is the more light and not add [] (like toString()
+            fileListString.append(fileList.get(i)).append(",");
+        }
+        fileListString.append(fileList.get(fileList.size() - 1));
+
+        /*
+         * File test editor (Write into file and read into file)
+         * 1- Write a file with data (string) and path
+         */
+        fileEditor.writeStringIntoFile(filePathOfIndexedFilePath, String.valueOf(fileListString)); // 1-Write data to a file given by the path
+        //TODO remove string transformation step
+
+    }
+
+    private static List<File> getFileListFromFileAsList(FileEditor fileEditor, String filePathOfIndexedFilePath) {
+        /*
+         * File test editor (Write into file and read into file)
+         * 2- Read the data in a file from path and get a String
+         * 3- Transform the String in a List<String> with split
+         * 4- Foreach the List<String> and for each item create a file with the path
+         */
+        String dataReadFromFile = fileEditor.readDataAsStringFile(filePathOfIndexedFilePath); // 2- Get the data in a file type string
+        String[] dataReadFromFileSplit = dataReadFromFile.split(","); //3-
+
+        List<File> fileListFromFile = new ArrayList<>(); //4
+        for (String item : dataReadFromFileSplit) {
+            fileListFromFile.add(new File(item));
+        }
+
+        return fileListFromFile;
+    }
 
     private static void testApp() {
         String filesToBeIndexedPath = "C:/Users/Daniel/Videos/Captures";
@@ -19,47 +59,43 @@ public class Main {
         //TODO test with wrong path
         //TODO make tests for whole project
 
-        /*
-         * File research test
-         * This part create a list of file searched
-         * And transform it into a String
-         */
         //System.out.println("current path: " + fileExplorer.getAppCurrentPath()); //Print current path
-        List<File> fileList = fileExplorer.getFilesInDirectories(fileExplorer.getDirectory()); //Save the path of wanted files
-        StringBuilder fileListString = new StringBuilder();
-        for (int i = 0; i < fileList.size() - 1; i++) { //Transform the list into string, many way tested, this is the more light and not add [] (like toString()
-            fileListString.append(fileList.get(i) + ",");
-        }
-        fileListString.append(fileList.get(fileList.size() - 1));
 
-        /*
-         * File test editor (Write into file and read into file)
-         * 1- Write a file with data (string) and path
-         * 2- Read the data in a file from path and get a String
-         * 3- Transform the String in a List<String> with split
-         * 4- Foreach the List<String> and for each item create a file with the path
-         */
-        fileEditor.writeStringIntoFile(filePathOfIndexedFilePath, String.valueOf(fileListString)); // 1-Write data to a file given by the path
-        String dataReadedFromFile = fileEditor.readDataAsStringFile(filePathOfIndexedFilePath); // 2- Get the data in a file type string
-        String[] dataReadedFromFileSplited = dataReadedFromFile.split(","); //3-
+        // writeFileListIntoFileAsString(fileExplorer, fileEditor, filePathOfIndexedFilePath);
+        //getFileListFromFileAsList(fileEditor, filePathOfIndexedFilePath);
 
-        List<File> fileListFromFile = new ArrayList<>(); //4
-        for (String item : dataReadedFromFileSplited) {
-            fileListFromFile.add(new File(item));
-        }
-
-        System.out.println(fileListFromFile);
         //TODO fix exception when path is wrong for write and read
         //TODO see with gson
     }
 
     public static void main(String[] args) {
-        testApp();
+        //testApp();
 
         /*
-        long startTime = System.currentTimeMillis(); //TIMER STARTTTTTTTTTTTTTTTTTT
-        long finishTime = System.currentTimeMillis(); //TIMER ENDDDDDDDDDDDDDDDDDDD
-        System.out.println("That took: " + (finishTime - startTime) + " ms"); //TIMER OUTTTTTTTTTTTTTTTTT
+        long startTime = System.currentTimeMillis(); //TIMER START
+        long finishTime = System.currentTimeMillis(); //TIMER END
+        System.out.println("That took: " + (finishTime - startTime) + " ms"); //TIMER OUT
         */
+
+        String filePathOfIndexedFilePath = "C:/Users/Daniel/Videos/Captures/filesPathIndexed.txt";
+        List<File> imagesPath = getFileListFromFileAsList(new FileEditor(), filePathOfIndexedFilePath);
+
+        ArrayList<String> imagesScanResult = new ArrayList<>();
+
+        Tesseract tesseract = new Tesseract();
+        tesseract.setDatapath("C:/Users/Daniel/Videos/Captures/tessdata");
+
+
+        try {
+            for (int i=0; i<imagesPath.size(); i++) { //not foreach loop, I need index
+                imagesScanResult.add(tesseract.doOCR(imagesPath.get(i)));
+                System.out.println(String.format("%d of %d scanned images",i,imagesPath.size()));
+            }
+        }
+        catch (TesseractException e){
+            e.printStackTrace();
+        }
+        System.out.println(imagesScanResult);
+
     }
 }
